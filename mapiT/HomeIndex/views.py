caf39a2a40f1f7de186ee47d2d3b6458be6cd675
MkshSahani <msco18333@gmail.com>
2020-09-08 17:06:04 +0530

    Singup Updated

--- mapiT/HomeIndex/views.py
+++ mapiT/HomeIndex/views.py
@@ -7,13 +7,19 @@ def indexHomePageRender(request):
     if request.method == "POST":
         username = request.POST.get("_username") # fetch the username.
         password = request.POST.get("_password")  # fetch the password.
-        _user = {
-            'username': username,
-            'password': password
-        }
-        print(_user)
-        context['username'] = username
-        context['password'] = password 
-        return render(request, 'HomePage.html', context) # need to change a lot..
+
+        if username is None:
+            print("<< === Signup requested... === >>")
+            return render(request, 'Hompage.html', context)
+        else:
+
+            _user = {
+                'username': username,
+                'password': password
+            }
+            print(_user)
+            context['username'] = username
+            context['password'] = password 
+            return render(request, 'HomePage.html', context) # need to change a lot..
     else:
         return render(request, 'HomePage.html', context) # render the Homage page without user login.
