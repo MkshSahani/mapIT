4a2f886d5faf45b8b0dec80b5e2a54d7fb3c91a2
MkshSahani <msco18333@gmail.com>
2020-09-10 10:30:27 +0530

    Sigup Form Updated

--- mapiT/HomeIndex/views.py
+++ mapiT/HomeIndex/views.py
@@ -4,13 +4,26 @@ from django.shortcuts import render
 
 def indexHomePageRender(request): 
     context = {}
+    ageList = [i for i in range(17, 90)]  # ageList
+    context['ageList'] = ageList
     if request.method == "POST":
         username = request.POST.get("_username") # fetch the username.
         password = request.POST.get("_password")  # fetch the password.
 
         if username is None:
             print("<< === Signup requested... === >>")
-            return render(request, 'Hompage.html', context)
+            username_ = request.POST.get("signupUserName")  # fetch the signup username
+            password_ = request.POST.get("signupUserPassword")  # fetch the password.
+            email_ = request.POST.get("signupEmail")  # fetch the user email.
+            city_ = request.POST.get("signupCity")  # fetch the user City.
+            age_ = request.POST.get("signupAge")
+            print(username_)
+            print(password_)
+            print(email_)
+            print(city_)
+            print(age_)
+
+            return render(request, 'HomePage.html', context)
         else:
 
             _user = {
